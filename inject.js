3ccd6457bd13909d0a77c0e6ac04b55e48006ea7
Martin Moe <martin@pepins.com>
2020-04-03 17:34:55 +0200

    Adding volume features

--- inject.js
+++ inject.js
@@ -4,63 +4,80 @@ function KeyController() {
     this.setup = function(videoEl) {
         this.keys = {
             space: 32,
+            enter: 13,
             arrowLeft: 37,
+            arrowUp: 38,
             arrowRight: 39,
+            arrowDown: 40,
             keyF: 70,
+            keyM: 77,
         };
         this.videoEl = videoEl,
         this.controlBar = document.querySelector('.vjs-control-bar');
-        this.forwardBtn = this.controlBar.querySelector('.forward-button');
-        this.backwardBtn = this.controlBar.querySelector('.backward-button');
         this.fullscreenBtn = this.controlBar.querySelector('.vjs-fullscreen-control');
         window.addEventListener("keydown", this.handleKeyPress);
     };
     this.handleKeyPress = event => {
-
         switch (event.keyCode) {
             case this.keys.space:
-                this.spacePress();
+            case this.keys.enter:
+                this.playPauseToggle();
                 break;
 
             case this.keys.arrowLeft:
-                this.arrowLeftPress();
+                this.seekVideo(-10);
                 break;
 
             case this.keys.arrowRight:
-                this.arrowRightPress();
+                this.seekVideo(+10);
                 break;
 
             case this.keys.keyF:
-                this.keyFPress();
+                this.fullscreenToggle();
+                break;
+
+            case this.keys.keyM:
+                this.muteToggle();
+                break;
+
+            case this.keys.arrowUp:
+                this.changeVolume(+0.1);
+                break;
+
+            case this.keys.arrowDown:
+                this.changeVolume(-0.1);
                 break;
         
             default:
                 break;
         }
     };
-    this.spacePress = () => {
+    this.playPauseToggle = () => {
         if(this.videoEl.paused) {
             this.videoEl.play();
         }else{
             this.videoEl.pause();
         }
     };
-    this.keyFPress = () => {
+    this.fullscreenToggle = () => {
         this.fullscreenBtn.click();
     };
-    this.arrowLeftPress = () => {
-        if(!this.backwardBtn) {
-            this.backwardBtn = this.controlBar.querySelector('.backward-button');
-        }
-        this.backwardBtn.click();
+    this.muteToggle = () => {
+        this.videoEl.muted = !this.videoEl.muted;
     };
-    this.arrowRightPress = () => {
-        if(!this.forwardBtn) {
-            this.forwardBtn = this.controlBar.querySelector('.forward-button');
+    this.seekVideo = (duration) => {
+        this.videoEl.currentTime += duration;
+    }
+    this.changeVolume = (addition) => {
+        const currentVolume = this.videoEl.volume;
+        let newVolume = currentVolume + addition;
+        if(newVolume > 0.9) {
+            newVolume = 1;
+        }else if(newVolume < 0.1) {
+            newVolume = 0;
         }
-        this.forwardBtn.click();
+        this.videoEl.volume = newVolume;
     };
-
 }
 
 
